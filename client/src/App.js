05b0b73a27546ed6c39986db75220ec87dd33b1a
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-11-25 18:17:45 +0100

    auth boiler plate

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AdminDashboard from "./pages/AdminDashboard";
+import Users from "./pages/Users";
+import Products from "./pages/Products";
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="app w-full h-full">
       <ToastContainer position="bottom-center" limit={1} />
       <Routes>
         <Route path="/" element={<AdminDashboard />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );
