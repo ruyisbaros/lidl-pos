1444045215a5bbb59646a8117b7dede26e713112
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-11-25 18:54:09 +0100

    page routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,20 @@ import "react-toastify/dist/ReactToastify.css";
 import AdminDashboard from "./pages/AdminDashboard";
 import Users from "./pages/Users";
 import Products from "./pages/Products";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div className="app w-full h-full">
       <ToastContainer position="bottom-center" limit={1} />
       <Routes>
-        <Route path="/" element={<AdminDashboard />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/" element={<AdminDashboard />}>
+          <Route path="/users" element={<Users />} />
+          <Route path="/products" element={<Products />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import NavbarMain from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+import Menu from "../components/navbar/Menu";
+import { Outlet } from "react-router-dom";
 
 const AdminDashboard = () => {
-  return <div>AdminDashboard</div>;
+  return (
+    <div className="main">
+      <NavbarMain />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default AdminDashboard;
