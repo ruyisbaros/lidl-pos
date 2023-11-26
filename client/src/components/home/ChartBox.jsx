925227cbc332f63853f753444d178763697578f2
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-11-26 20:50:40 +0100

    box chart

--- client/src/components/home/ChartBox.jsx
+++ client/src/components/home/ChartBox.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { LineChart, Line, Tooltip, ResponsiveContainer } from "recharts";
 
-const ChartBox = () => {
-  return <div>ChartBox</div>;
+const ChartBox = ({ dt }) => {
+  return (
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src={dt.icon} alt="" />
+          <span>{dt.title}</span>
+        </div>
+        <h1 className="chartNumber">{dt.number}</h1>
+        <Link to={`/${dt.dataKey}`} style={{ color: `${dt.color}` }}>
+          View all
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={dt.chartData}>
+              <Tooltip
+                contentStyle={{
+                  background: "transparent",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 60 }}
+              />
+              <Line
+                type="monotone"
+                dataKey={dt.dataKey}
+                stroke={dt.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="chartText">
+          <span
+            className="perc"
+            style={{ color: dt.percentage > 0 ? "tomato" : "limegreen" }}
+          >
+            {dt.percentage}%
+          </span>
+          <span className="duration">This Month</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChartBox;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import TopBox from "../components/home/TopBox";
+import ChartBox from "../components/home/ChartBox";
+import {
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../utils/data";
 
 const Home = () => {
   return (
@@ -7,11 +14,21 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">box2</div>
-      <div className="box box3">box3</div>
-      <div className="box box4">box4</div>
-      <div className="box box5">box5</div>
-      <div className="box box6">box6</div>
+      <div className="box box2">
+        <ChartBox dt={chartBoxUser} />
+      </div>
+      <div className="box box3">
+        {" "}
+        <ChartBox dt={chartBoxRevenue} />
+      </div>
+      <div className="box box4"> box4</div>
+      <div className="box box5">
+        <ChartBox dt={chartBoxProduct} />
+      </div>
+      <div className="box box6">
+        {" "}
+        <ChartBox dt={chartBoxConversion} />
+      </div>
       <div className="box box7">box7</div>
       <div className="box box8">box8</div>
       <div className="box box9">box9</div>
